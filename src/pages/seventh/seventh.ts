d2ac82642dfe1236f51c94188574319fdfcf921f
Viet Ngomai <thienvietngomai@gmail.com>
2018-08-09 21:55:50 -0400

    App now only lets you sign in with a jeffco email account! yayyyy the homepage develyn logo is fixed. also the login, logout, and alert methods are now more streamlined

--- src/pages/seventh/seventh.ts
+++ src/pages/seventh/seventh.ts
@@ -18,13 +18,12 @@ import firebase from 'firebase';
    templateUrl: 'seventh.html',
  })
  export class SeventhPage {
-   @ViewChild('username') user;
-   @ViewChild('password') password;
 
    provider = {
      name: '',
      profilePicture: '',
      email: '',
+     domain: '',
      loggedin: false
    }
 
@@ -32,9 +31,9 @@ import firebase from 'firebase';
      private alertCtrl: AlertController, public ref: ChangeDetectorRef) {
    }
 
-   alert(message: string){
+   alert(message: string, moniker = "Info!"){
      this.alertCtrl.create({
-       title: 'Info!',
+       title: moniker,
        subTitle: message,
        buttons: ['OK']
      }).present();
@@ -44,26 +43,38 @@ import firebase from 'firebase';
      console.log('ionViewDidLoad SeventhPage');
    }
 
-   loginWithGoogle(){
+   login(){
      this.fire.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
      .then( res => {
        console.log('From --Google--');
        console.log(res);
-       this.provider.loggedin = true;
        this.provider.name = res.user.displayName;
        this.provider.email = res.user.email;
        this.provider.profilePicture = res.user.photoURL;
-       this.alert('Success! You\'re logged in');
-       this.ref.detectChanges();
+       this.provider.domain = res.additionalUserInfo.profile.hd;
+       if(this.provider.domain != 'jeffcoschools.us'){
+         this.alert(this.provider.email+' isn\'t registered with jeffcoschools! Please log in with a jeffcoschools email.', 'Error!');
+         this.logout();
+       }else{
+         this.provider.loggedin = true;
+         this.alert('Success! You\'re logged in');
+         this.ref.detectChanges();  
+       }
+     })
+     .catch( error => {
+       console.log('got and error',error);
+       this.alert(error.message);
+       return false;
      })
    }
 
 
-
    logout(){
-     this.fire.auth.signOut();
+     this.fire.auth.signOut()
+     .then()
      this.provider.loggedin = false;
      this.ref.detectChanges();
    }
-
  }
+
+
